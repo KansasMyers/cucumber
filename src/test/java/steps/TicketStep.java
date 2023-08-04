f1950ab632301173f8fc93f1475897736d2e36a3
KansasMyers <kansasmyers360@gmail.com>
2023-08-04 20:46:07 -0300

    fix: Ajustando expressões regulares do cenário dos tickets.

--- src/test/java/steps/TicketStep.java
+++ src/test/java/steps/TicketStep.java
@@ -4,29 +4,24 @@ import io.cucumber.java.pt.Dado;
 
 public class TicketStep {
 	
-	@Dado("^que o ticket é (A.\\d{3})$")
-	public void queOTicketÉ(String string) {
-		System.out.println("Ticket: " + string);
+	@Dado("^que o ticket( especial)? é (A.\\d{3})$")
+	public void queOTicketE(String tipo, String ticket) {
+		System.out.println("Ticket: " + ticket);
 	}
 
-	@Dado("^que o ticket especial é (A.\\d{3})$")
-	public void queOTicketEspecialÉ(String string) {
-		System.out.println("Ticket Especial: " + string);
+	@Dado("^que o valor da passagem é R\\$ (\\d+[,]\\d+)$")
+	public void queOValorDaPassagemERS(Double valor) {
+		System.out.println("Valor: " + valor);
 	}
 
-	@Dado("^que o valor da passagem é [R][$] (\\d+[,]\\d+)$")
-	public void queOValorDaPassagemÉR$(Double double1) {
-		System.out.println("Valor: " + double1);
+	@Dado("^que o nome do passageiro é (.{5,20})$")
+	public void queONomeDoPassageiroE(String nomePassageiro) {
+		System.out.println("Nome do Passageiro: " + nomePassageiro);
 	}
 
-	@Dado("^que o nome do passageiro é (\\D+ \\D+ \\D+)$")
-	public void queONomeDoPassageiroÉ(String string) {
-		System.out.println("Nome do Passageiro: " + string);
-	}
-
-	@Dado("^que o telefone do passageiro é (9\\d{3}[-]\\d{4})$")
-	public void queOTelefoneDoPassageiroÉ(String string) {
-		System.out.println("Telefone: " + string);
+	@Dado("^que o telefone do passageiro é (9\\d{3}-\\d{4})$")
+	public void queOTelefoneDoPassageiroE(String telefone) {
+		System.out.println("Telefone: " + telefone);
 	}
 
 }
